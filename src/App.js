74509c748a453ccd8ead3173ad9d59dedc9c16c1
yasmeenattallah <attallah.a.yasmeen@gmail.com>
2025-04-01 16:43:11 +0200

    install routing and made a route for pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import {Home , AboutUs , Cart , Checkout, Contact, Login, NotFound, Order, ProductDetails, Product, Profile, Register , 
- } from './pages';
- import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  AboutUs,
+  Cart,
+  Checkout,
+  Contact,
+  Login,
+  NotFound,
+  Order,
+  ProductDetails,
+  Product,
+  Profile,
+  Register,
+} from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const App = ()=>  {
+const App = () => {
   return (
-    <div className="App">
-     <Home/> 
-     <AboutUs/>
-     <Cart/>
-     <Checkout/>
-     <Contact/>
-     <Login/>
-     <NotFound/>
-     <Order/>
-     <ProductDetails/>
-     <Product/>
-     <Profile/>
-     <Register/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      <Route path="/order" element={<Order />} />
+        <Route path="/prodDetails" element={<ProductDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/register" element={<Register />} />
+        <Route exact path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
 export default App;
